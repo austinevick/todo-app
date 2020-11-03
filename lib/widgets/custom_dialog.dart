7a3ff7f863892485e24fbfedd20d7bf67f27ddc2
Augustine Victor <augustinevickky@gmail.com>
2020-11-03 17:57:15 +0100

    Update custom_dialog.dart

--- lib/widgets/custom_dialog.dart
+++ lib/widgets/custom_dialog.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
 
 class CustomDialog extends StatelessWidget {
+  final Widget child;
+  final Animation<double> primaryAnimation;
+  final Animation<double> secondaryAnimation;
+
+  const CustomDialog({
+    Key key,
+    this.primaryAnimation,
+    this.secondaryAnimation,
+    this.child,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Transform.scale(
+        scale: primaryAnimation.value,
+        child: Opacity(
+          opacity: secondaryAnimation.value,
+          child: child,
+        ));
   }
 }
