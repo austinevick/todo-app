32104320f0f1cb6102f08cf307dccd48a1f2b9f4
Augustine Victor <augustinevickky@gmail.com>
2020-11-07 05:53:26 +0100

    Create note_screen.dart

---
+++ lib/screens/note_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NoteScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('All Note')),
+    );
+  }
+}
