991a098a2f4beb2dbe65b9c462cbf0dc8cc134eb
Augustine Victor <augustinevickky@gmail.com>
2020-11-08 20:58:43 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_app/provider/task_provider.dart';
 import 'package:todo_app/screens/bottom_navbar.dart';
-/*
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -16,133 +15,6 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'Quick Note',
         home: BottomNavBarScreen(),
-      ),
-    );
-  }
-}
-*/
-
-void main() => runApp(new MaterialApp(
-      theme: ThemeData(
-          appBarTheme: AppBarTheme(
-        color: Colors.red,
-      )),
-      home: new MyApp(),
-    ));
-
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-      FlutterLocalNotificationsPlugin();
-
-  @override
-  void initState() {
-    super.initState();
-    var initializationSettingsAndroid =
-        AndroidInitializationSettings('intropage');
-    var initializationSettingsIOs = IOSInitializationSettings();
-    var initSetttings = InitializationSettings(
-        initializationSettingsAndroid, initializationSettingsIOs);
-
-    flutterLocalNotificationsPlugin.initialize(initSetttings,
-        onSelectNotification: onSelectNotification);
-  }
-
-  Future onSelectNotification(String payload) {
-    Navigator.of(context).push(MaterialPageRoute(builder: (_) {
-      return NewScreen(
-        payload: payload,
-      );
-    }));
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: new AppBar(
-        backgroundColor: Colors.red,
-        title: new Text('Flutter notification demo'),
-      ),
-      body: new Center(
-        child: Column(
-          children: <Widget>[
-            RaisedButton(
-              onPressed: showNotification,
-              child: new Text(
-                'showNotification',
-              ),
-            ),
-            RaisedButton(
-              onPressed: cancelNotification,
-              child: new Text(
-                'cancelNotification',
-              ),
-            ),
-            RaisedButton(
-              onPressed: scheduleNotification,
-              child: new Text(
-                'scheduleNotification',
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Future<void> scheduleNotification() async {
-    var scheduledNotificationDateTime =
-        DateTime.now().add(Duration(seconds: 5));
-    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
-      'channel id',
-      'channel name',
-      'channel description',
-      icon: 'intropage',
-      largeIcon: DrawableResourceAndroidBitmap('intropage'),
-    );
-    var iOSPlatformChannelSpecifics = IOSNotificationDetails();
-    var platformChannelSpecifics = NotificationDetails(
-        androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
-    await flutterLocalNotificationsPlugin.schedule(
-        0,
-        'scheduled title',
-        'scheduled body',
-        scheduledNotificationDateTime,
-        platformChannelSpecifics);
-  }
-
-  Future<void> cancelNotification() async {
-    await flutterLocalNotificationsPlugin.cancel(0);
-  }
-
-  showNotification() async {
-    var android = new AndroidNotificationDetails(
-        'id', 'channel ', 'description',
-        priority: Priority.High, importance: Importance.Max);
-    var iOS = new IOSNotificationDetails();
-    var platform = new NotificationDetails(android, iOS);
-    await flutterLocalNotificationsPlugin.show(
-        0, 'Flutter devs', 'Flutter Local Notification Demo', platform,
-        payload: 'Welcome to the Local Notification demo ');
-  }
-}
-
-class NewScreen extends StatelessWidget {
-  String payload;
-
-  NewScreen({
-    @required this.payload,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(payload),
       ),
     );
   }
